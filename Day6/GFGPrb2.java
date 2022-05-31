f6bc6d451fd8459b220654c3c75b737cb17f409a
PorkodiVenkatesh <70228962+PorkodiVenkatesh@users.noreply.github.com>
2022-05-31 19:49:07 +0530

    Update GFGPrb2.java

--- Day6/GFGPrb2.java
+++ Day6/GFGPrb2.java
@@ -2,10 +2,14 @@ class Solution{
     int isdivisible7(String num){
         
         java.math.BigInteger holder = new java.math.BigInteger(num);
-       if (holder.mod(java.math.BigInteger.valueOf(7)) == java.math.BigInteger.ZERO)
-           return 1;
-        else
-            return 0;
+		java.math.BigInteger seven  = java.math.BigInteger.valueOf(7);
+		java.math.BigInteger rem = holder.mod(seven);
+		
+		if(rem == java.math.BigInteger.ZERO)
+			return 1;
+		else
+			return 0;
+
         
     }
 }
