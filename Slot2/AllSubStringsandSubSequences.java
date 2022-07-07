deb2e4f3c6f2f5578a57035fa4c5383efe798f53
PorkodiVenkatesh <70228962+PorkodiVenkatesh@users.noreply.github.com>
2022-07-07 15:57:53 +0530

    Add files via upload

---
+++ Slot2/AllSubStringsandSubSequences.java
@@ -0,0 +1,39 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class AllSubStringsandSubSequences {
+
+	 // Prints all subarrays in arr[0..n-1]
+    static void allSubString(String s)
+    {
+        int n = s.length();
+        // Pick starting point
+        for (int i=0; i <n; i++)
+        {
+            // Pick ending point
+            for (int j=i; j<n; j++)
+            {
+                // Print subarray between current starting
+                // and ending points
+                for (int k=i; k<=j; k++)
+                {
+                    System.out.print(s.charAt(k));
+                }
+                System.out.println();
+            }
+           
+        }
+    }
+    static void allSubSequence(String s) {
+    	//write a code to print all the subsequence
+    }
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println("::SubStrings::");
+		allSubString(s);
+		System.out.println("::SubSequence::");
+		allSubSequence(s);
+	}
+}
